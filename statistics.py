e19121c8e6f8791e69b81f9588bce86facfa5018
Sanjay Saatyaki <Saatyaki.Sanjay@in.bosch.com>
2021-10-19 17:13:51 +0530

    Impl fr test_raise_alerts_when_max_above_threshold

--- statistics.py
+++ statistics.py
@@ -8,5 +8,26 @@ def calculateStats(numbers):
     stats['avg'] = Average(numbers)
     stats['max'] = max(numbers)
     stats['min'] = min(numbers)
-
   return stats
+
+class EmailAlert:
+  def __init__(self):
+      self.emailSent = False
+
+class LEDAlert:
+  def __init__(self):
+      self.ledGlows = False
+
+class StatsAlerter:
+
+   def __init__(self,max_threshold,alerts):
+     self.max_threshold = max_threshold
+     self.alerts = alerts
+
+   def checkAndAlert(self,numbers):
+     calculate_Stats = calculateStats(numbers)
+
+     if calculate_Stats["max"] > self.max_threshold :
+       self.alerts[0].emailSent = True
+       self.alerts[1].ledGlows = True
+
--- statistics.test.py
+++ statistics.test.py
@@ -1,6 +1,7 @@
 import unittest
 import statistics
 import math
+from statistics import EmailAlert,LEDAlert,StatsAlerter
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
@@ -20,14 +21,14 @@ class StatsTest(unittest.TestCase):
     self.assertTrue(math.isnan(computedStats["max"]))
     self.assertTrue(math.isnan(computedStats["min"]))
 
-  # def test_raise_alerts_when_max_above_threshold(self):
-  #   emailAlert = EmailAlert()
-  #   ledAlert = LEDAlert()
-  #   maxThreshold = 10.5
-  #   statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
-  #   statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
-  #   self.assertTrue(emailAlert.emailSent)
-  #   self.assertTrue(ledAlert.ledGlows)
+  def test_raise_alerts_when_max_above_threshold(self):
+    emailAlert = EmailAlert()
+    ledAlert = LEDAlert()
+    maxThreshold = 10.5
+    statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
+    statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
+    self.assertTrue(emailAlert.emailSent)
+    self.assertTrue(ledAlert.ledGlows)
 
 if __name__ == "__main__":
   unittest.main()
